b717a614d85144381c8b6c9e71eaf2cce6bb94c2
wen <yellowwen@126.com>
2019-07-22 15:07:46 +0800

    test2

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/">Test</router-link>
+      <router-link to="/">Test</router-link> |
+      <router-link to="/">Test1</router-link>
     </div>
     <router-view/>
   </div>
